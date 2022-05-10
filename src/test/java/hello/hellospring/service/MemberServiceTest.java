0ddd2d874f646d651cf50e7bb7a0df49d43ce413
meatsby <meatsby@gmail.com>
2022-05-10 17:38:05 +0900

    study spring basics

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,53 @@
+package hello.hellospring.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.Test;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService(new MemoryMemberRepository());
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void 중복회원예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        assertThatThrownBy(() -> memberService.join(member2)).isInstanceOf(IllegalStateException.class)
+                .hasMessage("이미 존재하는 회원입니다.");
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
